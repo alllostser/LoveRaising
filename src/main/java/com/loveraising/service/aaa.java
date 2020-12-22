31cecaf73fe13d4cb2ed5aaa5625ce8d479d3b1a
guxinyu <975631766@qq.com>
2020-12-22 16:51:15 +0800

    项目框架

--- src/main/java/com/loveraising/service/aaa.java
+++ src/main/java/com/loveraising/service/aaa.java
@@ -9,12 +9,12 @@ import java.util.Map;
 /**
  * @program: biyesheji
  * @description:
- * @author: Guxinyu
- * @created: 2020/12/22 15:46
  */
 @Service
 public class aaa {
     @Autowired
+
+
 private com.loveraising.dao.test test;
     public List<Map<String, String>> aa (){
         return test.stringStringMap();
