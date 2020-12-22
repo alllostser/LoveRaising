f76b25976ee00890e2aa105ae400fae260da34cf
ChangHai <changh@yingxingroup.net>
2020-12-22 17:33:18 +0800

    22222

---
+++ src/main/java/com/loveraising/controller/UserInfoController.java
@@ -0,0 +1,13 @@
+package com.loveraising.controller;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("userinfo")
+public class UserInfoController {
+
+
+}
